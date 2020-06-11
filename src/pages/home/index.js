6dda59ebdd1c6b2e2f6aba1945d0e1de660b2e15
Yousef <yousefmohshallah33@gmail.com>
2020-06-11 10:50:07 +0300

    yousef [add] create product item

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ProductItem from '../../components/shared/product-item';
+import'./index.scss'
 
 class Home extends React.Component {
     render(){
         return(
             <div>
                 home
+                <ProductItem />
             </div>
         )
     }
